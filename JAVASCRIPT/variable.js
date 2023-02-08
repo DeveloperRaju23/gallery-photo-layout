b0d6da68c2aec90e5c313640da2ce11feb2f9b29
Developerraju23 <rajukumaraju.rk@gmail.com>
2023-02-08 13:42:09 +0600

    backend form design

---
+++ JAVASCRIPT/variable.js
@@ -0,0 +1,7 @@
+var price = 11;
+var age = 21;
+var temperature = 38;
+var taka = 210;
+var ojon = 72;
+var men = 4;
+var follower = 32333;
---
+++ PORTFOLIO/js/style.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed (".typedscrol",{
+    strings: ["FRONT END WEB DEVELOPER", "WEB DEVELOPER","PHOTO GRAPHER"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
